4fd68328d5f8f8f7cb9f933f792aad0a6f9f4a27
Khalid Shaikh <43288811+mkhalid-s@users.noreply.github.com>
2020-09-08 17:26:07 +0530

    Update SpringbootPostgreDemoApplicationTests.java

--- src/test/java/com/app/example/SpringbootPostgreDemoApplicationTests.java
+++ src/test/java/com/app/example/SpringbootPostgreDemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class SpringbootPostgreDemoApplicationTests {
 
 	@Test
